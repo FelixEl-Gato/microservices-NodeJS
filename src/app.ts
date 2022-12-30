c0caa7597ba8612ccdd22390cceb567f1c49af3f
FelixEl-Gato <tcin@outlook.es>
2022-12-30 13:07:06 -0500

    init project

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import * as express from "express";
+import * as cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:8080",
+      "http://localhost:4200",
+    ],
+  })
+);
+
+app.use(express.json());
+
+console.log("Server is running on port 8000");
+app.listen(8000);
